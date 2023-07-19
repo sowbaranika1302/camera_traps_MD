4882b57cac73613666f0009b968c83a6800c9ca0
Sowbaranika <balasubramaniam.29@osu.edu>
2023-07-19 14:53:41 -0400

    Update run_detector.py

--- run_detector.py
+++ run_detector.py
@@ -37,7 +37,28 @@ COORD_DIGITS = 4
 DEFAULT_DETECTOR_LABEL_MAP = {
     '1': 'animal',
     '2': 'person',
-    '3': 'vehicle'  # available in megadetector v4+
+    '3': 'vehicle',
+    '4': 'None',
+    '5': 'None',
+    '6': 'None',
+    '7': 'None',
+    '8': 'None',
+    '9': 'None',
+    '10': 'None',
+    '11': 'None',
+    '12': 'None',
+    '13': 'None',
+    '14': 'None',
+    '15': 'None',
+    '16': 'None',
+    '17': 'None',
+    '18': 'None',
+    '19': 'None',
+    '20': 'None',
+    '21': 'None',
+    '22': 'None',
+    '23': 'None'
+    # available in megadetector v4+
 }
 
 # Each version of the detector is associated with some "typical" values
