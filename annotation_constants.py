507913e70e095ee8653e06735e5a1f463f28db62
Sowbaranika <balasubramaniam.29@osu.edu>
2024-04-22 10:58:44 -0400

    Added categories

--- annotation_constants.py
+++ annotation_constants.py
@@ -11,31 +11,34 @@ We use integer indices here; this is different than the API output .json file,
 where indices are string integers.
 """
 
-NUM_DETECTOR_CATEGORIES = 3  # this is for choosing colors, so ignoring the "empty" class
+NUM_DETECTOR_CATEGORIES = 23  # this is for choosing colors, so ignoring the "empty" class
 
 # This is the label mapping used for our incoming iMerit annotations
 # Only used to parse the incoming annotations. In our database, the string name is used to avoid confusion
-annotation_bbox_categories = [
-    {'id': 0, 'name': 'empty'},
-    {'id': 1, 'name': 'animal'},
-    {'id': 2, 'name': 'person'},
-    {'id': 3, 'name': 'group'},  # group of animals
-    {'id': 4, 'name': 'vehicle'}
-]
-
-annotation_bbox_category_id_to_name = {}
-annotation_bbox_category_name_to_id = {}
-
-for cat in annotation_bbox_categories:
-    annotation_bbox_category_id_to_name[cat['id']] = cat['name']
-    annotation_bbox_category_name_to_id[cat['name']] = cat['id']
-
 # MegaDetector outputs
 detector_bbox_categories = [
-    {'id': 0, 'name': 'empty'},
-    {'id': 1, 'name': 'animal'},
-    {'id': 2, 'name': 'person'},
-    {'id': 3, 'name': 'vehicle'}
+   {"name": "Bird", "id": 0},
+    {"name": "Eastern Gray Squirrel", "id": 1},
+    {"name": "Eastern Chipmunk", "id": 2},
+    {"name": "Woodchuck", "id": 3},
+    {"name": "Wild Turkey", "id": 4},
+    {"name": "White-Tailed Deer", "id": 5},
+    {"name": "Virginia Opossum", "id": 6},
+    {"name": "Eastern Cottontail", "id": 7},
+    {"name": "Vehicle", "id": 9},
+    {"name": "Striped Skunk", "id": 10},
+    {"name": "Red Fox", "id": 11},
+    {"name": "Eastern Fox Squirrel", "id": 12},
+    {"name": "Northern Raccoon", "id": 13},
+    {"name": "Grey Fox", "id": 14},
+    {"name": "Horse", "id": 15},
+    {"name": "Dog", "id": 16},
+    {"name": "American Crow", "id": 17},
+    {"name": "Chicken", "id": 18},
+    {"name": "Domestic Cat", "id": 19},
+    {"name": "Coyote", "id": 20},
+    {"name": "Bobcat", "id": 21},
+    {"name": "American Black Bear", "id": 22}
 ]
 
 detector_bbox_category_id_to_name = {}
